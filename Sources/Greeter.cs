4290ef115adb73c106968e654a6d6d5a9908ec35
Alexandre Garino <alexandre.garino@odaceo.ch>
2015-12-12 14:02:35 +0100

    Use C# Coding Conventions

--- Sources/Greeter.cs
+++ Sources/Greeter.cs
@@ -4,7 +4,7 @@ namespace Odaceo
 {
     public class Greeter
     {
-        public static string sayHello(string name)
+        public static string SayHello(string name)
         {
             return String.Format("Hello, {0}!", name);
         }
